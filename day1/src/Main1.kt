b6a9971a163f02500c89db9d9734073f0f9cf026
Jeremy Harrault <jeremyharrault@hotmail.fr>
2023-12-03 10:37:27 +0100

    Day1 done - refactor

--- day1/src/Main1.kt
+++ day1/src/Main1.kt
@@ -2,10 +2,10 @@ import java.io.File
 
 fun main() {
     val inputLines = File("day1/resources/input.txt").readLines()
-    val sum = inputLines.map {
-        val firstDigit = it.first {it.isDigit() }
+    val sum = inputLines.sumOf {
+        val firstDigit = it.first { it.isDigit() }
         val lastDigit = it.last { it.isDigit() }
         "$firstDigit$lastDigit".toInt()
-    }.sum()
+    }
     println(sum)
 }
--- day1/src/Main2.kt
+++ day1/src/Main2.kt
@@ -3,9 +3,9 @@ import java.lang.Exception
 
 fun main() {
     val inputLines = File("day1/resources/input.txt").readLines()
-    val sum = inputLines.map {
+    val sum = inputLines.sumOf {
         getFirstDigit(it) * 10 + getLastDigit(it)
-    }.sum()
+    }
     println(sum)
 }
 
@@ -16,18 +16,8 @@ fun getFirstDigit(line: String) : Int {
         }
         else {
             line.subSequence(idx, line.length).let {
-                when {
-                    it.startsWith("one") -> return 1
-                    it.startsWith("two") -> return 2
-                    it.startsWith("three") -> return 3
-                    it.startsWith("four") -> return 4
-                    it.startsWith("five") -> return 5
-                    it.startsWith("six") -> return 6
-                    it.startsWith("seven") -> return 7
-                    it.startsWith("eight") -> return 8
-                    it.startsWith("nine") -> return 9
-                    else -> {}
-                }
+                val digit = getDigitFromName(it)
+                if (digit != 0) return digit
             }
         }
     }
@@ -41,20 +31,25 @@ fun getLastDigit(line: String): Int {
         }
         else {
             line.subSequence(line.length - 1 - idx, line.length).let {
-                when {
-                    it.startsWith("one") -> return 1
-                    it.startsWith("two") -> return 2
-                    it.startsWith("three") -> return 3
-                    it.startsWith("four") -> return 4
-                    it.startsWith("five") -> return 5
-                    it.startsWith("six") -> return 6
-                    it.startsWith("seven") -> return 7
-                    it.startsWith("eight") -> return 8
-                    it.startsWith("nine") -> return 9
-                    else -> {}
-                }
+                val digit = getDigitFromName(it)
+                if (digit != 0) return digit
             }
         }
     }
     throw Exception("Line is $line")
+}
+
+fun getDigitFromName(name: CharSequence): Int {
+    return when {
+        name.startsWith("one") -> 1
+        name.startsWith("two") -> 2
+        name.startsWith("three") -> 3
+        name.startsWith("four") -> 4
+        name.startsWith("five") -> 5
+        name.startsWith("six") -> 6
+        name.startsWith("seven") -> 7
+        name.startsWith("eight") -> 8
+        name.startsWith("nine") -> 9
+        else -> 0
+    }
 }
