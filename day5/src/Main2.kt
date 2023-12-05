19c21a9086f753e807e621068fd65c3edb8644df
damras <jeremyharrault@hotmai.fr>
2023-12-05 16:31:45 +0100

    Day5 done - refactor

--- day5/src/Main2.kt
+++ day5/src/Main2.kt
@@ -11,13 +11,13 @@ fun main() {
 
     val minimumLocation = almanac.seedRange
         .asSequence()
-        .flatMap { source -> getDestination(almanac.seedToSoil, source).also { println("seedToSoil: $source -> $it") } }
-        .flatMap { source -> getDestination(almanac.soilToFertilizer, source).also { println("soilToFertilizer: $source -> $it") } }
-        .flatMap { source -> getDestination(almanac.fertilizerToWater, source).also { println("fertilizerToWater: $source -> $it") } }
-        .flatMap { source -> getDestination(almanac.waterToLight, source).also { println("waterToLight: $source -> $it") } }
-        .flatMap { source -> getDestination(almanac.lightToTemperature, source).also { println("lightToTemperature: $source -> $it") } }
-        .flatMap { source -> getDestination(almanac.temperatureToHumidity, source).also { println("temperatureToHumidity: $source -> $it") } }
-        .flatMap { source -> getDestination(almanac.humidityToLocation, source).also { println("humidityToLocation: $source -> $it") } }
+        .flatMap { source -> getDestination(almanac.seedToSoil, source) }
+        .flatMap { source -> getDestination(almanac.soilToFertilizer, source) }
+        .flatMap { source -> getDestination(almanac.fertilizerToWater, source) }
+        .flatMap { source -> getDestination(almanac.waterToLight, source) }
+        .flatMap { source -> getDestination(almanac.lightToTemperature, source) }
+        .flatMap { source -> getDestination(almanac.temperatureToHumidity, source) }
+        .flatMap { source -> getDestination(almanac.humidityToLocation, source) }
         .toList()
         .minOf { it.first }
 
@@ -41,27 +41,17 @@ fun getDestination(currentMap: Map<LongRange, LongRange>, source: LongRange): Se
             destination.add(LongRange(destinationStart, destinationEnd))
 
             // HANDLE the remaining that is not part of the destination chunk.
-            // 3 cases are possible
-            when {
-                // * 1st case: the source starts before the destination chunk
-                // *** source:     |----------------|
-                // *** currentMap:         |------------|
-                source.first < it.key.first && source.last >= it.key.first && source.last <= it.key.last -> {
-                    destination.addAll(getDestination(currentMap, LongRange(source.first, it.key.first - 1)))
-                }
-                // * 2nd case: the source ends after the destination chunk
-                // *** source:           |----------------|
-                // *** currentMap: |--------------|
-                source.first >= it.key.first && source.first <= it.key.last && source.last > it.key.last -> {
-                    destination.addAll(getDestination(currentMap, LongRange(it.key.last + 1, source.last)))
-                }
-                // * 3rd case: the source starts before and ends after the destination chunk
-                // *** source:     |---------------------|
-                // *** currentMap:    |------------|
-                source.first < it.key.first && source.last > it.key.last -> {
-                    destination.addAll(getDestination(currentMap, LongRange(source.first, it.key.first - 1)))
-                    destination.addAll(getDestination(currentMap, LongRange(it.key.last + 1, source.last)))
-                }
+            // * 1st case: the source starts before the destination chunk
+            // *** source:     |----------------|
+            // *** currentMap:         |------------|
+            if (source.first < it.key.first) {
+                destination.addAll(getDestination(currentMap, LongRange(source.first, it.key.first - 1)))
+            }
+            // * 2nd case: the source ends after the destination chunk
+            // *** source:           |----------------|
+            // *** currentMap: |--------------|
+            if (source.last > it.key.last) {
+                destination.addAll(getDestination(currentMap, LongRange(it.key.last + 1, source.last)))
             }
         }
     }
